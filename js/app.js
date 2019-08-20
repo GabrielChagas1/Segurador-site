dfee0e7c0bb35afd08c27b0e885e379f9ed02d75
Gabriel Chagas <gabrielserq@outlook.com>
2019-08-20 16:51:25 -0300

    finalizando form de contato

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.border-icons').click(function(){
+        // window.location.href('/form.html');
+        // var novaURL = "file:///C:/Users/Gabriel%20Chgas/Desktop/Mander";
+
+        // $(location).attr('href', novaURL +'/form.html');
+        $('#form-contato').modal();
+    });
+});
